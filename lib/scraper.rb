aa7966766a881f51b4c69efc2433bc860ab3118c
anthony mcmullen <jondouglasheit@gmail.com>
2020-04-30 08:40:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
     x = 0
     while x <student_details.length
     index.css("div.student-card").each do |student|
-      student_details = {}
+      student_details =[]
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
       profile_path = student.css("a").attribute("href").value
