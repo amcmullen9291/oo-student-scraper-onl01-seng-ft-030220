f4acebce418f9c1e9c25dd185dc45905fbeff635
anthony mcmullen <jondouglasheit@gmail.com>
2020-04-30 08:38:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,8 +16,8 @@ class Scraper
       profile_path = student.css("a").attribute("href").value
       student_details[:profile_url] = './fixtures/student-site/' + profile_path
       students << student_details[x]
-      x+=1
     end
+    x += 1
     students
   end
 
