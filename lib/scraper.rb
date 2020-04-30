592e6ebd04ccfbd73b428082362a8ec110737c2c
anthony mcmullen <jondouglasheit@gmail.com>
2020-04-30 08:27:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     students = []
-    html = open(index_url)
+    html = UriOpen(index_url)
     index = Nokogiri::HTML(html)
     index.css("div.student-card").each do |student|
       student_details = {}
